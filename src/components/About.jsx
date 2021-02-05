707254966c3be3e0f999194e428815c229929e94
WKapenda <wkapenda@gmail.com>
2021-02-05 11:50:03 +0200

    About section, added profile picture, text amendments to be completed

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,21 +7,26 @@ function About(){
 
         <h2>Who's this guy?</h2>
         
-            <div class="row">
-                <div class="col-lg-6 col-sm-6 col-xs-6 py-2">
-                    <img src=""></img>
+            <div class="row aboutPicDes">
+                <div class="col-lg-6 col-sm-12 col-xs-6 py-2">
+                    <img src={require("./images/profilePhoto.png")} class="profilePic"></img>
                 </div>
 
-                <div class="col-lg-6 col-sm-6 col-xs-6 py-2"> 
-
+                <div class="col-lg-6 col-sm-12 col-xs-6 py-2"> 
                     <p>
-                    I'm a web developer with over 2 years of professional experience. I am interested in all kind of visual 
-                    communication but my major focus is on creating mobile, tablet and desktop interfaces. I also have skills in 
-                    other fields like computer hardwares, hardware troubleshooting, skateboarding and gaming. Earlier I was a full-time
-                    freelancer and also have been an intern at LiteStack a local tech start-up. Currently, I'm currently open for as a 
-                    web developer
-                    </p>
 
+                    My name is Willem Kapenda, I hold a degree in Electrical Engineering (Hon) from the University of the Witwatersrand and 
+                    I am aspiring to become a world class Software Engineer. I currently have three (3) years of working experience at a Zinc Ore Refinery 
+                    and a one (1) year internship working at an engineering consulting company.
+                    I am interested and keen to continue developing my career in the field of Software Engineering because I have always been interested in 
+                    software development, possessing exceptional math and computer skills. I improve my skills by doing relevant online courses on Udemy, 
+                    Coursera and DataCamp. Achieving the following certificates: Tensorflow 2.0: Deep Learning and Artificial Intelligence, SQL for Data Science 
+                    and Python Programming.I am fluent in these programming languages: Python, JavaScript, and C++. 
+                    As an Electrical Engineer In Training, I have demonstrated strong problem-solving skills and the ability to work in high-pressure and 
+                    challenging working environments. I am detail-oriented, organized, and precise in my work. I have strong communication skills with a knack for clear and 
+                    illuminating presentation. I’m comfortable on my own facing the numbers, but I really enjoy being part of a motivated team of smart individuals.
+
+                    </p>
                 </div>
 
             </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,150 +7,82 @@ function Skills(){
     return(
     <div class="techStack">
 
-    <h2> My Tech Stack </h2>
+        <h2> My Tech Stack </h2>
+
+            <div class="row techIcons">
+                <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
+                    <i class="fab fa-html5 boxshadow" style={{color: "#E44D26"}}></i>
+                        <h6>HTML</h6>
+                </div>
+
+                <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
+                    <i class="fab fa-css3-alt boxshadow" style={{color: "#2965f1"}}></i>
+                        <h6>CSS</h6>
+                </div>
+
+                <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
+                    <i class="fab fa-js boxshadow" style={{color: "#f0db4f"}}></i>
+                        <h6>JAVASCRIPT</h6>
+                </div>
+
+                <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
+                    <i class="fab fa-react boxshadow" style={{color: "#61DBFB"}}></i>
+                        <h6>REACT.JS</h6>
+                </div>
+            </div>
 
         <div class="row">
 
             <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
-                <div class="boxshadow">
-                    <Icon 
-                        path={techIcons.mdiLanguageHtml5}
-                        title="User Profile"
-                        size={3}
-                        color= "#E44D26"
-                    />
-                    <h6>HTML</h6>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-sm-6 col-xs-6 py-2"> {/* shado */}
-                <div class="boxshadow">
-                <Icon path={techIcons.mdiLanguageCss3}
-                        title="User Profile"
-                        size={3}
-                        color="#2965f1"
-                    />
-                    <h6>CSS</h6>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-sm-6 col-xs-6 py-2 ">
-                <div class="boxshadow">
-                <Icon path={techIcons.mdiLanguageJavascript}
-                        title="User Profile"
-                        size={3}
-                        color="#f0db4f"
-                    />
-                    <h6>JAVASCRIPT</h6>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
-                <div class="boxshadow">
-                <Icon path={techIcons.mdiReact}
-                        title="User Profile"
-                        size={3}
-                        color="#61DBFB"
-                    />
-                    <h6>REACT.JS</h6>
-                </div>
-            </div>
-
-        </div>
-
-        <div class="row">
-
-            <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
-                <div class="boxshadow">
-                <Icon path={techIcons.mdiNodejs}
-                        title="User Profile"
-                        size={3}
-                        color="#6cc24a"
-                    />
+                <i class="fab fa-node-js boxshadow" style={{color: "#6cc24a"}}></i>
                     <h6>NODE.JS</h6>
-                </div>
-                </div>
+            </div>
 
-                <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
-                <div class="boxshadow">
-                <Icon path={techIcons.mdiDatabase}
-                        title="User Profile"
-                        size={3}
-                        color="#00758F"
-                    />
+            <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
+                <i class="fas fa-database boxshadow" style={{color: "#03506f"}}></i>
                     <h6>SQL</h6>
-                </div>
-                </div>
+            </div>
 
-                <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
-                <div class="boxshadow">
-                <Icon path={techIcons.mdiLeaf}
-                        title="User Profile"
-                        size={3}
-                        color="#4DB33D"
-                    />
+            <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
+                <i class="fab fa-envira boxshadow" style={{color: "#4DB33D"}}></i>
                     <h6>MONGODB</h6>
-                </div>
-                </div>
+            </div>
 
-                <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
-                <div class="boxshadow">
-                <Icon path={techIcons.mdiGit}
-                        title="User Profile"
-                        size={3}
-                        color="#F1502F"
-                    />
+            <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
+                <i class="fab fa-git-alt boxshadow" style={{color: "#F1502F"}}></i>
                     <h6>GIT</h6>
-                </div>
             </div>
 
         </div>
 
         <div class="row">
 
-        <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
-            <div class="boxshadow">
-            <Icon path={techIcons.mdiBootstrap}
-                        title="User Profile"
-                        size={3}
-                        color= "purple"
-                    />
-                <h6>BOOTSRAP4</h6>
-            </div>
+            <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
+                <i class="fab fa-bootstrap boxshadow" style={{color: "purple"}}></i>
+                    <h6>BOOTSRAP4</h6>
             </div>
 
             <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
-            <div class="boxshadow">
-            <Icon path={techIcons.mdiJquery}
-                        title="User Profile"
-                        size={3}
-                        color="#0769AD"
-                    />
+                {/* <div class=""> */}
+                <Icon 
+                className = "jqueryIcon boxshadow"
+                path={techIcons.mdiJquery}
+                title="User Profile"
+                size={3}
+                color="#0769AD"
+                />     
+                {/* </div> */}
                 <h6>JQUERY</h6>
             </div>
+
+            <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
+                <i class="fas fa-cogs boxshadow" style={{color: "black"}}></i>
+                    <h6>RESTful APIs</h6>
             </div>
 
             <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
-            <div class="boxshadow">
-            <Icon path={techIcons.mdiApi}
-                        title="User Profile"
-                        size={3}
-                        color="black"
-                    />
-                <h6>REST</h6>
-            </div>
-            </div>
-
-            <div class="col-lg-3 col-sm-6 col-xs-6 py-2">
-            <div class="Animate bg-image hover-zoom boxshadow">
-            <Icon path={techIcons.mdiAlphaHBoxOutline}
-                        title="User Profile"
-                        horizontal
-                        size={3}
-                        color="purple"
-                    />
-                <h6>HEROKU</h6>
-            </div>
+                <i class="fas fa-h-square boxshadow" style={{color: "purple"}}></i>
+                    <h6>HEROKU</h6>
             </div>
 
         </div>
