3792f3aa75d3d2ce14d1f77390b6b2986001f8c7
WKapenda <wkapenda@gmail.com>
2021-03-01 17:19:20 +0200

    Navbar padding adjusted, and background color fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,25 @@
 import React, { Component, useState} from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
 
+// Drawer dependencies
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+
+
+
 
 function Header(){
 
@@ -10,29 +29,117 @@ function Header(){
 
   const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
+  // Drawer functions
+
+  const drawerWidth = 240;
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: "flex"
+    },
+    appBar: {
+      transition: theme.transitions.create(["margin", "width"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      })
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(["margin", "width"], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen
+      })
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    hide: {
+      display: "none"
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0
+    },
+    drawerPaper: {
+      width: drawerWidth
+    },
+    drawerHeader: {
+      display: "flex",
+      alignItems: "center",
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: "flex-end"
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+      }),
+      marginLeft: -drawerWidth
+    },
+    contentShift: {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen
+      }),
+      marginLeft: 0
+    }
+  }));
+
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+
 
   return(
-  
-    <nav class="Nav navbar navbar-light bg-light container-fluid sticky-top navbar-expand-lg">
-      <Link
+
+    <div className={classes.root}>
+    <CssBaseline/>
+    <AppBar
+      position="fixed"
+      className="${clsx(classes.appBar, {
+        [classes.appBarShift]: open
+      })} Nav"
+      
+    >
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          className={clsx(classes.menuButton, open && classes.hide) }
+          
+        >
+        <MenuIcon />
+
+        </IconButton>
+        <Link
             className="logo navbar-brand btn"
             alt="Logo"
-            onClick={scrollToTop}
-            // type="button"
-            
+            onClick={scrollToTop} 
         >
         <img src={require("./images/wklogo.png")} class="wklogo"></img>
-      </Link>
+        </Link>
 
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
-      aria-controls="navbarSupportedContent" aria-expanded={isNavCollapsed ? true : false} aria-label="Toggle navigation" 
-      onClick={handleNavCollapse}>
-        <span class="navbar-toggler-icon"></span></button>
-      <div className= {`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
+        <div className="navlinks  ml-auto">
+
+
           <Link
-                className="nav-link btn"
+                className="btn"
                 activeClass="active"
                 to="section1"
                 smooth={true}
@@ -42,10 +149,9 @@ function Header(){
               >
                 HOME
           </Link>
-          </li>
-          <li class="nav-item">
+
           <Link
-                className="nav-link btn"
+                className="btn"
                 activeClass="active"
                 to="section2"
                 smooth={true}
@@ -55,10 +161,9 @@ function Header(){
               >
                 SKILLS
           </Link>
-          </li>
-          <li class="nav-item">
+
           <Link
-                className="nav-link btn"
+                className="btn"
                 activeClass="active"
                 to="section3"
                 smooth={true}
@@ -68,10 +173,9 @@ function Header(){
               >
                 PROJECTS
           </Link>
-          </li>
-          <li class="nav-item">
+
           <Link
-                className="nav-link btn"
+                className="btn"
                 activeClass="active"
                 to="section4"
                 smooth={true}
@@ -81,11 +185,10 @@ function Header(){
               >
                 ABOUT
           </Link>
-          </li>
-          <li class="nav-item nav-btn">
+
           <Link
-                className="nav-link btn btn-primary"
-                type="button"
+                className="btn contact-btn btn-primary"
+                // type="button"
                 // activeClass="active"
                 to="section5"
                 // spy={true}
@@ -95,10 +198,51 @@ function Header(){
               >
                 CONTACT ME
           </Link>
-          </li>
-        </ul>
+          </div>
+      </Toolbar>
+
+    </AppBar>
+    <Drawer
+      className={classes.drawer}
+      variant="persistent"
+      anchor="left"
+      open={open}
+      classes={{
+        paper: classes.drawerPaper
+      }}
+    >
+      <div className="${classes.drawerHeader}  Nav">
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === "ltr" ? (
+            <ChevronLeftIcon />
+          ) : (
+            <ChevronRightIcon />
+          )}
+        </IconButton>
       </div>
-    </nav>
+      <Divider />
+      <List>
+        {["Home", "Skills", "Projects", "About", "Contact Me"].map(
+          (text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          )
+        )}
+      </List>
+    </Drawer>
+    <main
+      className={clsx(classes.content, {
+        [classes.contentShift]: open
+      })}
+    >
+      <div className={classes.drawerHeader} />
+    </main>
+  </div>
+
+   
+  
+
 
   );
 }
